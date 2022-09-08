e0a171e32199c5d0df23ece5dedfd720210b0527
wideraHannes <h.widera@mammutmedia.eu>
2022-09-08 12:13:04 +0200

    Main Section finished

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <main-section title="Home" :loading="loading">
+            this is our Home Page
+        </main-section>
+    </div>
+</template>
+<script setup>
+const loading = ref(false)
+</script>
