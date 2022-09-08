565da5161ee147dc0fad054efc410125cef819a9
wideraHannes <h.widera@mammutmedia.eu>
2022-09-08 17:12:02 +0200

    added User and DB setup, next: Transformer

--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -1,5 +1,6 @@
 // post /api/auth/register
 import {sendError} from "h3"
+import { createUser } from "~~/server/db/users"
 
 export default defineEventHandler(async (event) => {
     const body = await useBody (event)
@@ -14,7 +15,19 @@ export default defineEventHandler(async (event) => {
             return sendError(event, createError({statusCode: 400,
             statusMessage: 'invalid Params'}))
         }
+    if (password !== repeatPassword) {
+        return sendError(event, createError({statusCode: 400,
+            statusMessage: 'Password do not match'}))
+    }
+    const userData = {
+        username,
+        email,
+        password,
+        name,
+        profileImage: 'https://picsum.photos/200/200'
+    }
+    const user = await createUser(userData)
     return {
-        body
+        user
     }
 })
